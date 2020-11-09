c73343808247cb82fd18fc6efbcf2c3653aef647
p.chatchai <kmitl50ite15engineer@gmail.com>
2020-11-09 09:11:36 +0700

    ****

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:qbmatic/screens/main_rider.dart';
 import 'package:qbmatic/screens/main_shop.dart';
@@ -19,10 +20,16 @@ class _HomeState extends State<Home> {
     // TODO: implement initState
     super.initState();
     checkPreference();
+
+    initFirebaseMessaging();
   }
 
   Future<Null> checkPreference() async {
     try {
+      FirebaseMessaging firebaseMessaging = FirebaseMessaging();
+      String token = await firebaseMessaging.getToken();
+      print("token <====> $token");
+
       SharedPreferences preferences = await SharedPreferences.getInstance();
       String chooseType = preferences.getString('ChooseType');
       if (chooseType != null && chooseType.isNotEmpty) {
@@ -41,6 +48,35 @@ class _HomeState extends State<Home> {
     }
   }
 
+  void initFirebaseMessaging() {
+    FirebaseMessaging firebaseMessaging = FirebaseMessaging();
+    // String token = await firebaseMessaging.getToken();
+    // print("token <====> $token");
+    firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+
+    firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+
+    firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      print("Token : $token");
+    });
+  }
+
   void routeToService(Widget myWidget) {
     MaterialPageRoute route = MaterialPageRoute(builder: (context) => myWidget);
     Navigator.pushAndRemoveUntil(context, route, (route) => false);
@@ -49,7 +85,9 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('QBMATIC'),),
+      appBar: AppBar(
+        title: Text('QBMATIC'),
+      ),
       drawer: showDrawer(),
     );
   }
